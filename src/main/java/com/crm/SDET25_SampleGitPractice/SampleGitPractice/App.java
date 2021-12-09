84f32709ae6c1cd7e29a2d784e0a47dc98953d01
L340 <L340@LAPTOP-D6FDH20G>
2021-12-09 15:50:34 +0530

    it was just for a practice purpose

---
+++ src/main/java/com/crm/SDET25_SampleGitPractice/SampleGitPractice/App.java
@@ -0,0 +1,13 @@
+package com.crm.SDET25_SampleGitPractice.SampleGitPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/crm/SDET25_SampleGitPractice/SampleGitPractice/AppTest.java
@@ -0,0 +1,20 @@
+package com.crm.SDET25_SampleGitPractice.SampleGitPractice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
