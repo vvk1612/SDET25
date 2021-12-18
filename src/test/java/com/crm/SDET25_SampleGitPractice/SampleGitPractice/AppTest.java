fd59c20a6b6ecbce54884a364c2c39e1e349cb3e
v <v@Lenovo>
2021-12-18 17:34:14 +0530

    hdjydh

--- src/test/java/com/crm/SDET25_SampleGitPractice/SampleGitPractice/AppTest.java
+++ src/test/java/com/crm/SDET25_SampleGitPractice/SampleGitPractice/AppTest.java
@@ -1,20 +1,13 @@
 package com.crm.SDET25_SampleGitPractice.SampleGitPractice;
 
-import static org.junit.Assert.assertTrue;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+       System.out.println("sssvhfssssss");
     }
 }
